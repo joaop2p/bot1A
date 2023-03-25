fa522dae3aa2700c37c44042cdca6b0fc742f3b4
João Paulo <62178699+joaop2p@users.noreply.github.com>
2023-03-25 17:10:38 -0300

    bot.py
    Adicionado uma interface para selecionar arquivos.

--- bot.py
+++ bot.py
@@ -1,29 +1,28 @@
 # Importei as bibliotecas
-
 import pandas as pd
 import pyautogui as py
 import time
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
+Tk().withdraw() # para esconder a janela principal
+NomeArquivo = askopenfilename() # mostra uma caixa de diálogo para o usuário selecionar um arquivo
 # Pegando uma tebela, transformando tudo em string para poder digitar e colocar todos os itens de uma coluna em uma lista
-df = pd.DataFrame(pd.read_csv('Dados.csv'))
+df = pd.DataFrame(pd.read_csv(NomeArquivo))
 ordem = df.sort_values(by='UC')
 df = ordem.applymap(str)
 lista = df['NCT'].tolist()
-
 # Obtendo a quantidade de repetição com base na quantidade de itens
-total = len(lista)
 QuantidadeFeita = len(lista)
-i = 0
-
-time.sleep(5)
-# Loop para realizar a digitação
-while i <= total:
-    py.write(lista[0])
-    py.press('enter')
-    lista.pop(0)
-    i = i + 1
-    if i == total:
-        print('acabou')
-        break
-print('finalizado! Foram Realizadas: ', QuantidadeFeita, ' preenchimentos.')
-time.sleep(5)
+resposta = input('Você tem certeza que deseja realizar toda a digitação desse aquivo?')
+if resposta == 'S':
+    print('Você tem 5 segundos para colocar no local em quer realizar a digitação')
+    time.sleep(5)
+    # Loop para realizar a digitação
+    for item in lista:
+        py.write(item)
+        py.press('enter')
+    print('finalizado! Foram Realizadas:', QuantidadeFeita, ' preenchimentos.')
+    time.sleep(5)
+else:
+    exit()
