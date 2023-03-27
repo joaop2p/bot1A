3e6fc0a0cc7be2b15f040fe4f4eb918367f0cbd4
João Paulo <62178699+joaop2p@users.noreply.github.com>
2023-03-26 22:55:42 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ Tk().withdraw()
 while True:
 # mostra uma caixa de diálogo para o usuário selecionar um arquivo
     NomeArquivo = askopenfilename()
-
     # Pegando uma tebela, transformando tudo em string para poder digitar e colocar todos os itens de uma coluna em uma lista
     df = pd.DataFrame(pd.read_csv(NomeArquivo))
     ordem = df.sort_values(by='UC')
@@ -19,18 +18,21 @@ while True:
 
     # Obtendo a quantidade de repetição com base na quantidade de itens
     QuantidadeFeita = len(lista)
-
     resposta = input('Você tem certeza que deseja realizar toda a digitação desse aquivo?[S/N]')
 
     if resposta == 'S':
         print('Você tem 5 segundos para colocar no local em quer realizar a digitação')
         time.sleep(5)
         
+        start_time = time.time()
         # Loop para realizar a digitação
         for item in lista:
             py.write(item)
             py.press('enter')
+        end_time = time.time()
+        elapsed_time = end_time - start_time
         print('finalizado! Foram Realizadas:', QuantidadeFeita, ' preenchimentos.')
+        print(f'Tempo de execução: {elapsed_time:.2f} segundos')
         time.sleep(5)
 
     else:
